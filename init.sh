7b5da54e9e0917281a97835d6b4da16868da01a1
macmacs <macmacs@users.noreply.github.com>
2017-09-16 16:54:47 +0200

    Added init_script

---
+++ init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# clean up
+oc delete all -l "app=letsencrypt"
+
+if [ -z "$LETSENCRYPT_CONTACT_EMAIL" ]; then
+    echo "Need to set LETSENCRYPT_CONTACT_EMAIL"
+    exit 1
+fi 
+
+oc adm policy add-role-to-user edit -z letsencrypt
+
+# Create clusterrole letsencrypt (if not existing)
+oc get clusterrole letsencrypt > /dev/null 2>&1
+    if ! [ "$?" -eq 0 ]; then
+		oc create -f letsencrypt-clusterrole.yaml
+		oc adm policy add-cluster-role-to-user letsencrypt system:serviceaccount:`oc project -q`:letsencrypt
+	fi
+
+# STAGING
+LETSENCRYPT_CA="https://acme-staging.api.letsencrypt.org/directory"
+# LIVE
+# LETSENCRYPT_CA="https://acme-v01.api.letsencrypt.org/directory"
+
+oc process -f template.yaml \
+	-p LETSENCRYPT_CONTACT_EMAIL=${LETSENCRYPT_CONTACT_EMAIL} \
+	-p LETSENCRYPT_CA=${LETSENCRYPT_CA} \
+	| oc create -f -
