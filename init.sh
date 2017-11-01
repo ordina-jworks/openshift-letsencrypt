5d0d5e14bcf7ca4aed4405a4ef0d87dea2653aec
root <root@timbuktuu.rothmitha.org>
2017-11-01 12:15:53 +0100

    Project name automatically added to volume claim

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # clean up
 oc delete all -l "app=letsencrypt"
@@ -9,13 +10,17 @@ if [ -z "$LETSENCRYPT_CONTACT_EMAIL" ]; then
 fi 
 
 # STAGING
-LETSENCRYPT_CA="https://acme-staging.api.letsencrypt.org/directory"
+# LETSENCRYPT_CA="https://acme-staging.api.letsencrypt.org/directory"
 # LIVE
-# LETSENCRYPT_CA="https://acme-v01.api.letsencrypt.org/directory"
+LETSENCRYPT_CA="https://acme-v01.api.letsencrypt.org/directory"
+
+PROJECT=`oc project -q`
+echo $PROJECT
 
 oc process -f template.yaml \
 	-p LETSENCRYPT_CONTACT_EMAIL=${LETSENCRYPT_CONTACT_EMAIL} \
 	-p LETSENCRYPT_CA=${LETSENCRYPT_CA} \
+	-p PROJECT=${PROJECT} \
 	| oc create -f -
 
 oc adm policy add-role-to-user edit -z letsencrypt
